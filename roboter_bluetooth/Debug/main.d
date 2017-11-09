87408b05febb42c8b97a5266fbe5e39aa0669707
Staud14 <Staud14@users.noreply.github.com>
2017-11-09 13:11:38 +0100

    Debugging

--- roboter_bluetooth/Debug/main.d
+++ roboter_bluetooth/Debug/main.d
@@ -1,6 +1,4 @@
-header/bluetooth_header/roboter_bluetooth.d \
- header/bluetooth_header/roboter_bluetooth.o: \
- ../header/bluetooth_header/roboter_bluetooth.c \
+main.d main.o: .././main.c \
  f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
@@ -12,8 +10,14 @@ header/bluetooth_header/roboter_bluetooth.d \
  f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
  f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- ../header/bluetooth_header/roboter_bluetooth.h
+ f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ .././header/bluetooth_header/roboter_bluetooth.h \
+ .././header/drive_header/roboter_drive.h \
+ .././header/I2C_header/roboter_I2C.h
 
 f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -37,6 +41,18 @@ f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\includ
 
 f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
+f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
 f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
-../header/bluetooth_header/roboter_bluetooth.h:
+f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+f:\programms\atmel\ studio\ 7.0\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+.././header/bluetooth_header/roboter_bluetooth.h:
+
+.././header/drive_header/roboter_drive.h:
+
+.././header/I2C_header/roboter_I2C.h:
--- roboter_bluetooth/main.c
+++ roboter_bluetooth/main.c
@@ -10,23 +10,30 @@
 #include <avr/io.h>
 #include <string.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
+
+#define TRUE            1
+#define FALSE           0
 
 #include "header/bluetooth_header/roboter_bluetooth.h"
 #include "header/drive_header/roboter_drive.h"
 #include "header/I2C_header/roboter_I2C.h"
 
 unsigned char bReceive = 0;
+volatile unsigned char akkuLevel, newAkkuLevel, firstCheck;
 
-struct adctobyte
+
+union ADC_frame
 {
-	unsigned int L : 8;
-	unsigned int U : 2;
-}
-union ADC
-{
-	unsigned int in;
-	struct adctobyte out 10;
-}
+	unsigned int input;
+	
+	struct output
+	{
+		unsigned char lsb;
+		unsigned char msb;
+	}OUTPUT;
+	
+};
 
 
 
@@ -72,10 +79,10 @@ int main(void)
 			bReceive = 0;
 		}
 		{
-			union ADC adc;
-			adc.in = (0b00000001 << 8) + 0b00000000;
+			union ADC_frame adc;
+			adc.input = (0b00000001 << 8) + 0b00000000;
 			//adc.in = adc_measure(MEASURE_UB);
-			bprintf(adc.out.L);
+			bprintf(adc.OUTPUT.lsb);
 		}
 	}
 }
@@ -84,4 +91,70 @@ int main(void)
 ISR(USART1_RX_vect)
 {
 	bReceive = UDR1;
+}
+
+ISR(ADC_vect)
+{
+	unsigned int wait;
+	unsigned char x, y;
+
+	newAkkuLevel = ADCH;
+	if (akkuLevel > newAkkuLevel)
+	akkuLevel = newAkkuLevel;
+
+	if (firstCheck)
+	{
+		firstCheck = FALSE;
+
+		PORTB |= (1<<LED_LV)|(1<<LED_LH);
+		PORTB |= (1<<LED_RV)|(1<<LED_RH);
+
+
+		if (akkuLevel > 210)
+		{
+			x = (akkuLevel - 200) / 5;
+
+			PORTB &= ~(1 << LED_GRUEN);
+			for (y = 0; y < x; y++)
+			{
+				for(wait = 0; wait < 60000; wait++)  PORTB |=  (1 << LED_GRUEN);
+				for(wait = 0; wait < 60000; wait++)  PORTB &= ~(1 << LED_GRUEN);
+			}
+		}
+		else if (akkuLevel > 205)
+		{
+
+			for(wait = 0; wait < 60000; wait++)
+			{
+				PORTB |= (1 << LED_ROT);
+				PORTB |= (1 << LED_GRUEN);
+			}
+		}
+		else
+		{
+			for(wait = 0; wait < 60000; wait++)  PORTB |= (1 << LED_ROT);
+		}
+		PORTB &= ~(1 << LED_GRUEN);
+		PORTB &= ~(1 << LED_ROT);
+	}
+
+	if (akkuLevel < 200)
+	{
+
+		TCCR4B &= 0xF0;		//alle Motoren stopp, Motor PWM aus
+		PORTB  &= 0x30;		//alle LEDs am Ring aus, Beeper und Motor right aus
+		PORTD  =  0x08;		//Motor left und alle IR-Sender aus
+
+		cli();
+		while(1)
+		{
+			for(wait = 0; wait < 60000; wait++)  PORTB |= (1 << LED_ROT);
+			for(wait = 0; wait < 40000; wait++)  PORTB &= ~(1 << LED_ROT);
+		}
+		PORTB &= ~(1<<LED_LV) & ~(1<<LED_LH);
+		PORTB &= ~(1<<LED_RV) & ~(1<<LED_RH);
+
+	}
+
+
 }
